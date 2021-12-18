19942032d11bd9825604776122ef494b598a411e
stiwin123 <stiwin@hotmail.com>
2021-12-18 15:34:16 -0500

    shared_preferences chat
    guardar datos local en el chat

--- lib/domain/models/controller.dart
+++ lib/domain/models/controller.dart
@@ -1,11 +1,18 @@
+import 'package:blockchain_pal/pages/Landing.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:blockchain_pal/pages/chat.dart';
 
 class MyConttroller extends GetxController {
   var _change = false.obs;
   var _change1 = false.obs;
+  var _texto = "11111".obs;
+  var _colorchat = 0xffffffff.obs;
 
   bool get change => _change.value;
   bool get change1 => _change.value;
+  String get texto => _texto.value;
+  int get colorchat => _colorchat.value;
 
   void cambioChange() {
     _change.value = _change.value == true ? false : true;
@@ -13,5 +20,15 @@ class MyConttroller extends GetxController {
 
   void cambioChange1() {
     _change1.value = _change1.value == true ? false : true;
+  }
+
+  void cambioTexto(String colorTexto) {
+    //SharedPreferences prefs = await SharedPreferences.getInstance();
+    _texto.value = colorTexto;
+    //prefs.setString("value", value);
+  }
+
+  void cambioColorContainer(int colorContainer) {
+    _colorchat.value = colorContainer;
   }
 }
--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,10 +1,35 @@
+//import 'dart:html';
+//import 'dart:html';
 import 'dart:io';
+import 'package:blockchain_pal/domain/models/controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:blockchain_pal/pages/Ingresado.dart';
 import 'package:blockchain_pal/pages/chatfeed.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 
-class chat extends StatelessWidget {
+class chat extends StatefulWidget {
+  @override
+  State<chat> createState() => _chatState();
+}
+
+class _chatState extends State<chat> {
+  final mensaje = TextEditingController();
+  String value = '';
+  int value2 = 2;
+
+  String men = '';
+  int col = 0xffffffff;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _cargarPreferencias2();
+  }
+
   @override
   Widget build(BuildContext context) {
     final focusedCtx = FocusManager.instance.primaryFocus!.context;
@@ -16,7 +41,14 @@ class chat extends StatelessWidget {
             ));
     return Scaffold(
       appBar: buildAppBar(context),
-      body: buildBody1(),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          child: buildBody(),
+        ),
+      ),
     );
   }
 
@@ -93,118 +125,170 @@ class chat extends StatelessWidget {
   }
 
   buildBody() {
-    return Column(
-      children: [
-        Container(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                const SizedBox(height: 30),
-                Container(
-                  width: 450,
-                  height: 450,
-                  //color: Colors.orange,
-                ),
-                SingleChildScrollView(
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: "Mensaje",
-                        hintStyle: TextStyle(
-                            fontSize: 17,
-                            color: Color.fromRGBO(157, 159, 160, 1)),
-                        fillColor: Color(0xfff6f7fa),
-                        filled: true,
-                        //border: InputBorder.none,
-                        prefixIcon: Icon(
-                          Icons.emoji_emotions_outlined,
-                          color: Color(0xff826AD9),
-                          size: 28,
-                        ),
-                        suffixIcon: Icon(
-                          Icons.send,
-                          color: Color(0xff826AD9),
-                          size: 28,
+    //SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.setString("value", men);
+
+    MyConttroller myConttroller = Get.find();
+    final textController = TextEditingController();
+    return ListView(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(height: 30),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                child: Image.asset('images/perfil5.png',
+                                    height: 40),
+                              ),
+                              Container(
+                                  decoration: BoxDecoration(
+                                    color: Color(0xff826ad9),
+                                    borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(40.0),
+                                      bottomLeft: Radius.circular(40.0),
+                                    ),
+                                  ),
+                                  width: 300,
+                                  height: 80,
+                                  //color: Colors.blue,
+                                  child: Center(
+                                      child: Text(
+                                    "Hola!, puedo preguntarte algo?, \n necesito tu ayuda",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 17),
+                                  ))
+                                  //textAlign: TextAlign.start,
+                                  ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Container(
+                                  decoration: BoxDecoration(
+                                    color: Color(myConttroller.colorchat),
+                                    borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(40.0),
+                                      bottomLeft: Radius.circular(40.0),
+                                    ),
+                                  ),
+                                  width: 300,
+                                  height: 80,
+                                  //color: Colors.blue,
+                                  child: Center(
+                                      child: Obx(
+                                    () => Text(
+                                      myConttroller.texto,
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 17),
+                                    ),
+                                  ))
+                                  //textAlign: TextAlign.start,
+                                  ),
+                              Container(
+                                child: Image.asset('images/perfil.png',
+                                    height: 40),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      width: 450,
+                      height: 450,
+                      color: Colors.white,
+                    ),
+                    SingleChildScrollView(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
+                        child: TextField(
+                          controller: mensaje,
+
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            hintText: "Mensaje",
+                            hintStyle: TextStyle(
+                                fontSize: 17,
+                                color: Color.fromRGBO(157, 159, 160, 1)),
+                            fillColor: Color(0xfff6f7fa),
+                            filled: true,
+                            //border: InputBorder.none,
+                            prefixIcon: Icon(
+                              Icons.emoji_emotions_outlined,
+                              color: Color(0xff826AD9),
+                              size: 28,
+                            ),
+                            suffixIcon: IconButton(
+                                icon: Icon(Icons.send),
+                                color: Color(0xff826AD9),
+                                onPressed: () => {
+                                      men = mensaje.text,
+                                      //print(men),
+                                      mensaje.clear(),
+                                      myConttroller.cambioTexto(men),
+                                      myConttroller.cambioColorContainer(col),
+                                      _cargarPreferencias1(),
+                                      SystemChannels.textInput
+                                          .invokeMethod('TextInput.hide'),
+                                    }
+
+                                //size: 28,
+                                ),
+                          ),
+                          minLines:
+                              1, // any number you need (It works as the rows for the textarea)
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
                         ),
                       ),
-                      minLines:
-                          1, // any number you need (It works as the rows for the textarea)
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          ],
+        )
       ],
     );
   }
-}
 
-buildBody1() {
-  return ListView(
-    children: <Widget>[
-      Column(
-        children: [
-          Container(
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  const SizedBox(height: 30),
-                  Container(
-                    width: 450,
-                    height: 450,
-                    //color: Colors.orange,
-                  ),
-                  SingleChildScrollView(
-                    child: Container(
-                      margin: EdgeInsets.only(
-                          top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          hintText: "Mensaje",
-                          hintStyle: TextStyle(
-                              fontSize: 17,
-                              color: Color.fromRGBO(157, 159, 160, 1)),
-                          fillColor: Color(0xfff6f7fa),
-                          filled: true,
-                          //border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.emoji_emotions_outlined,
-                            color: Color(0xff826AD9),
-                            size: 28,
-                          ),
-                          suffixIcon: Icon(
-                            Icons.send,
-                            color: Color(0xff826AD9),
-                            size: 28,
-                          ),
-                        ),
-                        minLines:
-                            1, // any number you need (It works as the rows for the textarea)
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      )
-    ],
-  );
+  _cargarPreferencias1() async {
+    //para guardar el valor en shared_preferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    value = men;
+    prefs.setString("value", value);
+
+    //int value2 = 0;
+    //value2 = col;
+    //prefs.setInt("value1", value2);
+  }
+
+  _cargarPreferencias2() async {
+    //para llamar el valor guardado en shared_preferences (initestate)
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    MyConttroller myConttroller = Get.find();
+    String _value3 = "";
+    int value4 = 0;
+
+    _value3 = prefs.getString("value") ?? '';
+    myConttroller.cambioTexto(_value3);
+
+    //value4 = prefs.getInt("value1")!;
+
+    //myConttroller.cambioColorContainer(value4);
+  }
 }
